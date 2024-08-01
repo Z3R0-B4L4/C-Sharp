05b7291380735f55deadf33400ef7050b4fb6bd9
Z3R0-B4L4 <eduardo9685743210@gmail.com>
2024-08-01 17:40:57 -0300

    teste53

---
+++ Teste53/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Teste53
+{
+    partial class F_Main
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_nome = new System.Windows.Forms.TextBox();
+            this.btn_adicionar = new System.Windows.Forms.Button();
+            this.tb_listaNomes = new System.Windows.Forms.TextBox();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.btn_show = new System.Windows.Forms.Button();
+            this.btn_valNum = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.componentesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkboxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkedlistToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite um Nome:";
+            // 
+            // tb_nome
+            // 
+            this.tb_nome.Location = new System.Drawing.Point(12, 84);
+            this.tb_nome.Name = "tb_nome";
+            this.tb_nome.Size = new System.Drawing.Size(154, 20);
+            this.tb_nome.TabIndex = 1;
+            this.tb_nome.TextChanged += new System.EventHandler(this.tb_nome_TextChanged);
+            // 
+            // btn_adicionar
+            // 
+            this.btn_adicionar.Location = new System.Drawing.Point(172, 82);
+            this.btn_adicionar.Name = "btn_adicionar";
+            this.btn_adicionar.Size = new System.Drawing.Size(75, 23);
+            this.btn_adicionar.TabIndex = 2;
+            this.btn_adicionar.Text = "Adicionar";
+            this.btn_adicionar.UseVisualStyleBackColor = true;
+            this.btn_adicionar.Click += new System.EventHandler(this.btn_adicionar_Click);
+            // 
+            // tb_listaNomes
+            // 
+            this.tb_listaNomes.Location = new System.Drawing.Point(12, 110);
+            this.tb_listaNomes.Multiline = true;
+            this.tb_listaNomes.Name = "tb_listaNomes";
+            this.tb_listaNomes.ReadOnly = true;
+            this.tb_listaNomes.Size = new System.Drawing.Size(235, 235);
+            this.tb_listaNomes.TabIndex = 3;
+            this.tb_listaNomes.TextChanged += new System.EventHandler(this.tb_listaNomes_TextChanged);
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Location = new System.Drawing.Point(12, 352);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(235, 23);
+            this.btn_limpar.TabIndex = 4;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // btn_show
+            // 
+            this.btn_show.Location = new System.Drawing.Point(12, 382);
+            this.btn_show.Name = "btn_show";
+            this.btn_show.Size = new System.Drawing.Size(235, 23);
+            this.btn_show.TabIndex = 5;
+            this.btn_show.Text = "Mostrar Nomes";
+            this.btn_show.UseVisualStyleBackColor = true;
+            this.btn_show.Click += new System.EventHandler(this.btn_show_Click);
+            // 
+            // btn_valNum
+            // 
+            this.btn_valNum.Location = new System.Drawing.Point(12, 412);
+            this.btn_valNum.Name = "btn_valNum";
+            this.btn_valNum.Size = new System.Drawing.Size(235, 23);
+            this.btn_valNum.TabIndex = 6;
+            this.btn_valNum.Text = "Valor da variavel num";
+            this.btn_valNum.UseVisualStyleBackColor = true;
+            this.btn_valNum.Click += new System.EventHandler(this.btn_valNum_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.componentesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // componentesToolStripMenuItem
+            // 
+            this.componentesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkboxToolStripMenuItem,
+            this.checkedlistToolStripMenuItem});
+            this.componentesToolStripMenuItem.Name = "componentesToolStripMenuItem";
+            this.componentesToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.componentesToolStripMenuItem.Text = "Componentes";
+            // 
+            // checkboxToolStripMenuItem
+            // 
+            this.checkboxToolStripMenuItem.Name = "checkboxToolStripMenuItem";
+            this.checkboxToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkboxToolStripMenuItem.Text = "Checkbox";
+            this.checkboxToolStripMenuItem.Click += new System.EventHandler(this.checkboxToolStripMenuItem_Click);
+            // 
+            // checkedlistToolStripMenuItem
+            // 
+            this.checkedlistToolStripMenuItem.Name = "checkedlistToolStripMenuItem";
+            this.checkedlistToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkedlistToolStripMenuItem.Text = "Checkedlistbox";
+            this.checkedlistToolStripMenuItem.Click += new System.EventHandler(this.checkedlistToolStripMenuItem_Click);
+            // 
+            // F_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_valNum);
+            this.Controls.Add(this.btn_show);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.tb_listaNomes);
+            this.Controls.Add(this.btn_adicionar);
+            this.Controls.Add(this.tb_nome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "F_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Componentes";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_nome;
+        private System.Windows.Forms.Button btn_adicionar;
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.Button btn_show;
+        private System.Windows.Forms.Button btn_valNum;
+        public System.Windows.Forms.TextBox tb_listaNomes;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem componentesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkboxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkedlistToolStripMenuItem;
+    }
+}
+
---
+++ Teste53/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Teste53
+{
+    public partial class F_Main : Form
+    {
+        public int num;
+        public F_Main()
+        {
+            InitializeComponent();
+            num = 0;
+        }
+
+        private void tb_nome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_adicionar_Click(object sender, EventArgs e)
+        {
+            
+
+            if(tb_nome.Text == "")
+            {
+                MessageBox.Show("Insira um nome");// mensagem de erro caso o campo tb_nome esteja vazio
+                tb_nome.Focus();// foca no campo tb_nome
+                return;
+            }
+            else
+            {
+                tb_listaNomes.Text += tb_nome.Text + "\r\n"; // adiciona o nome no campo tb_listanomes
+                tb_nome.Clear();// limpa o campo tb_nome
+                tb_nome.Focus();// foca no campo tb_nome
+            }
+        }
+
+        private void tb_listaNomes_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            tb_listaNomes.Clear();// limpa o campo tb_listanomes
+            tb_nome.Clear();// limpa o campo tb_nome
+            tb_nome.Focus();// foca no campo tb_nome
+        }
+
+        private void btn_show_Click(object sender, EventArgs e)
+        {
+            F_Nomes f_Nomes = new F_Nomes(tb_listaNomes.Text, this); // cria uma instância da classe F_Nomes
+            f_Nomes.ShowDialog();// mostra a instância da classe F_Nomes
+        }
+
+        private void btn_valNum_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(num.ToString());
+        }
+
+        private void componenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cheToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkboxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            F_CheckBox f_CheckBox = new F_CheckBox();
+            f_CheckBox.ShowDialog();
+        }
+
+        private void checkedlistToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            F_CheckedListBox f_CheckedListBox = new F_CheckedListBox();
+            f_CheckedListBox.ShowDialog();
+        }
+    }
+}
