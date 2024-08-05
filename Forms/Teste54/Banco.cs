0423eeb0e44139cc5c9d0da51ce34d06dde35593
Z3R0-B4L4 <eduardo9685743210@gmail.com>
2024-08-04 22:46:45 -0300

    Inicio Teste54, criado conexão com o banco de dados 'teste54'

---
+++ Forms/Teste54/Banco.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Teste54
+{
+    internal class Banco
+    {
+
+        private static MySqlConnection con;//connection
+
+        private static MySqlConnection Connect() 
+        {
+            con = new MySqlConnection("datasource=localhost;port=3306;username=root;password=/*/*;database=teste54;");
+            con.Open();
+            return con;
+        }
+        
+    }
+}
---
+++ Forms/Teste54/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Teste54
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lb_user = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb_acess = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pb_ledLogado = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ledLogado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.lb_user);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lb_acess);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pb_ledLogado);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 427);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 23);
+            this.panel1.TabIndex = 0;
+            // 
+            // lb_user
+            // 
+            this.lb_user.AutoSize = true;
+            this.lb_user.Location = new System.Drawing.Point(182, 5);
+            this.lb_user.Name = "lb_user";
+            this.lb_user.Size = new System.Drawing.Size(10, 13);
+            this.lb_user.TabIndex = 4;
+            this.lb_user.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Usuario:";
+            // 
+            // lb_acess
+            // 
+            this.lb_acess.AutoSize = true;
+            this.lb_acess.Location = new System.Drawing.Point(75, 5);
+            this.lb_acess.Name = "lb_acess";
+            this.lb_acess.Size = new System.Drawing.Size(13, 13);
+            this.lb_acess.TabIndex = 2;
+            this.lb_acess.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Acesso:";
+            // 
+            // pb_ledLogado
+            // 
+            this.pb_ledLogado.Image = global::Teste54.Properties.Resources.circulo;
+            this.pb_ledLogado.Location = new System.Drawing.Point(3, 3);
+            this.pb_ledLogado.Name = "pb_ledLogado";
+            this.pb_ledLogado.Size = new System.Drawing.Size(15, 15);
+            this.pb_ledLogado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_ledLogado.TabIndex = 0;
+            this.pb_ledLogado.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Teste54 - v1.0";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ledLogado)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.PictureBox pb_ledLogado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_acess;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb_user;
+    }
+}
+
